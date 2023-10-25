99b9c37792b751712e878ee7e1d713aa395d2391
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-25 09:55:37 +0200

    variable

--- programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
+++ programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
@@ -1,9 +1,6 @@
-// FirstConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* FirstConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-#include <iostream>
 
-/*
 
    Ctrl + D = linijka w dol
    Ctrl + K + D = formatowanie kodu
@@ -12,7 +9,11 @@
    \t = odstep
    aby wypchnac na GitHub trzeba wejsc w git changes po prawej na gorze napisac cos w message i wybrac opcje commit all and push zamiast commit all
 
+   prawy przycisk myszy, set as startup project
+
 */
+
+#include <iostream>
 
 int main() {
 	std::cout << "Hello World!\n";
@@ -21,16 +22,18 @@ int main() {
 	std::cout << 'D';
 	std::cout << '\n';
 	std::cout << 69420 + 1 << "\n";   //Daj¹c drugi raz << mozemy w 1 linii pisac kilka slow
-	std::cout << 12.5 << "\n";
+	std::cout << 12.5 << "\n";       //liczby 12 i pol zapisujemy 12.5 zamiast 12,5
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+ /*
+ Run program : Ctrl + F5 or Debug > Start Without Debugging menu
+ Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+ Tips for Getting Started: 
+   1. Use the Solution Explorer window to add/manage files
+   2. Use the Team Explorer window to connect to source control
+   3. Use the Output window to see build output and other messages
+   4. Use the Error List window to view errors
+   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+   */
---
+++ programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -0,0 +1,94 @@
+/*
+prawy przycisk myszy, set as startup project
+*/
+#include <iostream>
+
+// napisz program, ktory wczyta od uzytkownika jego wiek a nastepnie wyswietli go na konsoli
+
+void zadanie() {
+    short wiek;
+    std::cout << "Podaj swoj wiek. \n";
+    std::cin >> wiek;                               // std::cin >> (zmienna);  -  pobiera zmienn¹
+    std::cout << wiek;
+
+
+    short liczba1 = 90;
+    short liczba2;
+    std::cout << "Podaj liczbe \n";
+    std::cin >> liczba2;
+    std::cout << liczba1 + liczba2;
+}
+
+void zadanie2() {
+
+    float m;
+
+    std::cout << "Podaj dlugosc w metrach";
+    std::cin >> m;
+
+    float cm = m * 100;
+    float mm = m * 1000;
+    float km = m / 1000;
+
+
+}
+
+int main()
+{
+    //zadanie();
+    zadanie2();
+}
+
+/*
+
+ Algorytm -  zbiór instrukcji realizuj¹cy postawione przed nim zadanie.
+ Sposoby zapisu:
+
+ - W punktach
+ - Opis s³owny
+ - Jêzyk programowania lub pseudokod
+ - Zbiór rysunków
+ - Schemat blokowy
+
+ Zmienna - jest to pewien obszar w pamieci, sluzacy do przechowywania danych. Te dane mozna modyfikowac.
+ Zmienna pozwala przechowac tylko 1 wartosc w danym momencie czasu.
+
+ Deklaracja zmiennej - moment jej utworzenia.
+
+ typ_zmiennej nazwa_zmiennej;
+
+ Typ zmiennej - mowi o wielkosci obszaru pamieci i o rodzaju przechowywanych danych.
+
+ short         liczby calkowite ze znakiem       2 bajty      <-32 768; 32 767>
+ int/long      liczby calkowite ze znakiem       4 bajty      <-2 147 483 648; 2 147 483 647>
+ long long     liczby calkowite ze znakiem       8 bajtow     <-9 223 372 036 854 775 808, 9 223 372 036 854 775 807>
+
+
+ Jezeli przed powyzszymi typami dodamy slowo "unsigned" to zakres
+ jest od zera do podwojonego zakresu gornego plus 1.
+
+
+ float          liczby rzeczywiste ze znakiem       4 bajty
+ double         liczby rzeczywiste ze znakiem       8 bajtow
+ long double    liczby rzeczywiste ze znakiem       12 bajtow
+
+ Nazwa zmiennej - nazwa obszaru w pamieci. Poprzez ta nazwe odwolujemy sie do tego obszaru.
+
+ Wymagania kompilatora wzgledem nazwy:
+ * uzycie tylko dozwolonych znakow 
+ - alfabet angielski a-z oraz A-Z
+ -cyfry arabskie 0-9
+ -podloga _
+ * pierwszym znakiem nie moze byc cyfra
+ * nie moze to byc slowo kluczowe danego jezyka programowania
+ * musi byc unikalna w danej widocznosci
+
+ Wymagania programistow:
+ * jesli nazwa sklada sie z wielu wyrazow to zamiast spacji:
+ - uzywamy znaku podkreslenia
+ - stosujemy camel case, czyli piszemy wszystko razem a slowa (zaczynamy od drugiego)
+ zaczynamy z duzej litery, np. firstNumberFromUser
+ * nazwa zmiennej musi oddawac charakter przechowywanych danych, czyli miec znaczenie
+ * nazwy po angielsku!!
+
+*/
