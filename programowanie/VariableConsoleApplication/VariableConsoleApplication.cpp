51a90a04c02f48adcbef11e082b52f8e2e427ac3
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-11-08 09:55:53 +0100

    cos

--- programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -1,46 +1,112 @@
 /*
 prawy przycisk myszy, set as startup project
 */
+
+
+/*
+
+                                                                       * ZADANIA *
+
+
+* Napisz program, który poprosi u¿ytkownika o podanie promienia ko³a i obliczy pole tego ko³a, wyœwietlaj¹c wynik.
+
+* Napisz program, który poprosi u¿ytkownika o podanie d³ugoœci podstawy a, podstawy b i wysokoœci h trapezu, a nastêpnie obliczy pole tego trapezu, wyœwietlaj¹c wynik.
+
+* Napisz program, który poprosi u¿ytkownika o podanie d³ugoœci krawêdzi szeœcianu i obliczy jego objêtoœæ, wyœwietlaj¹c wynik.
+
+* Napisz program, który poprosi u¿ytkownika o podanie temperatury w stopniach Celsiusza, a nastêpnie przeliczy j¹ na stopnie Fahrenheita i wyœwietli wynik.
+
+* Napisz program, który konwertuje wprowadzon¹ kwotê w jednej walucie na inn¹, korzystaj¹c z okreœlonego kursu wymiany.
+
+* Napisz program, który poprosi u¿ytkownika o podanie dwóch liczb (a, b) i obliczy œredni¹ arytmetyczn¹ tych liczb. Nastêpnie wyœwietli wynik.
+
+* Napisz program, który poprosi u¿ytkownika o podanie trzech liczb (a, b, c) i obliczy œredni¹ arytmetyczn¹ tych liczb. Nastêpnie wyœwietli wynik.
+
+* Program obliczaj¹cy œredni¹ wa¿on¹ trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+
+* Napisz program, który poprosi u¿ytkownika o podanie dwóch liczb ca³kowitych. Nastêpnie wykona operacje (+, -, *, /) i wyœwietli wyniki.
+
+* Napisz program, który poprosi u¿ytkownika o podanie liczby sekund i przeliczy je na godziny i minuty, wyœwietlaj¹c wynik.
+
+* Napisz program, który poprosi u¿ytkownika o podanie wspó³rzêdnych (x1, y1) i (x2, y2) dwóch punktów w uk³adzie kartezjañskim, a nastêpnie obliczy odleg³oœæ miêdzy nimi, wyœwietlaj¹c wynik.
+
+
+*/
+
+
+
 #include <iostream>
 
-// napisz program, ktory wczyta od uzytkownika jego wiek a nastepnie wyswietli go na konsoli
+int main() {
+	setlocale(LC_CTYPE, "polish");
+	//wiek();
+	//metry();
+	//pole();
+	trojkat();
+}
 
-void zadanie() {
+void wiek() 
+{
+	// napisz program, ktory wczyta od uzytkownika jego wiek a nastepnie wyswietli go na konsoli
 	short wiek;
 	std::cout << "Podaj swoj wiek. \n";
-	std::cin >> wiek;                               // std::cin >> (zmienna);  -  pobiera zmienn¹
+	std::cin >> wiek;
 	std::cout << "masz " << wiek << " lat. \n";
 }
-void zadanie2() {
+void metry() 
+{
 	float m;
 	std::cout << "Podaj dlugosc w metrach \n";
 	std::cin >> m;
 
-
 	float cm = m * 100;
 	float mm = cm * 10;
 	float km = m / 1000;
-
 
 	std::cout << "Metry: " << m << "\n";
 	std::cout << "Centymetry: " << cm << "\n";
 	std::cout << "Milimetry: " << mm << "\n";
 	std::cout << "Kilometry: " << km << "\n";
 }
-void test() {
-	short liczba1 = 30/2;
-	short liczba2;
-	std::cout << "Podaj liczbe \n";
-	std::cin >> liczba2;
-	std::cout << liczba1 + liczba2;
+
+void pole() 
+{
+	//Napisz program, który poprosi u¿ytkownika o podanie d³ugoœci boku a i b prostok¹ta, a nastêpnie obliczy i wyœwietli pole prostok¹ta.
+	float bok1, bok2;	
+	std::cout << "Podaj bok a \n";
+	std::cin >> bok1;	
+	std::cout << "Podaj bok b \n";
+	std::cin >> bok2;
+	std::cout << "Pole prostokata wynosi: " << bok1 * bok2;
 }
 
-int main()
+int trojkat()
 {
-	//zadanie();
-	//test();
-	//zadanie2();
+	//Napisz program, który poprosi u¿ytkownika o podanie d³ugoœci podstawy i wysokoœci trójk¹ta, a nastêpnie obliczy pole tego trójk¹ta, wyœwietlaj¹c wynik.
+	float a, h;
+	std::cout << "Podaj dlugosc podstawy trojkata";
+	std::cin >> a;
+	std::cout << "Podaj wysokosc trojkata";
+	std::cin >> h;
+	std::cout << "Pole trojkata wynosi: " << a / 2 * h;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 
